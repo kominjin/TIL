6bc23dc422c95415c7c02f1011705547f0c37bcc
kominjin <mjko415@naver.com>
2022-03-23 18:00:22 +0900

    4

---
+++ example.py
@@ -0,0 +1,16 @@
+def calculate_rectangle_area(x, y):
+    return x*y
+x=10
+y=10
+print("사각형의 x의 길이는 :", x)
+print("사각형의 y의 길이는 :", y)
+
+result = calculate_rectangle_area(x, y)
+print("사각형의 넓이는", result)
+
+def f(x):
+    return 2*x+7
+def g(x):
+    return x ** 2
+x=2
+print( f(x)+g(x)+f(g(x))+g(f(x))))
